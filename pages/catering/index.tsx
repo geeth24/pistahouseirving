05d97c874af201c8a03943ca1eb95ba29b5277c7
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-07-18 23:23:11 -0500

    update image layout

--- pages/catering/index.tsx
+++ pages/catering/index.tsx
@@ -19,6 +19,7 @@ import { logEvent } from "firebase/analytics"
 import { analytics } from "@/components/Firebase"
 import Head from "next/head"
 import { useRouter } from "next/router"
+import Image from "next/image"
 
 const renderPhoto: RenderPhoto = ({
     layout,
@@ -135,44 +136,44 @@ const images = [
     },
 ]
 function Catering() {
-     const [logged, setLogged] = React.useState(false)
-     const router = useRouter()
-     const { order } = router.query
+    const [logged, setLogged] = React.useState(false)
+    const router = useRouter()
+    const { order } = router.query
 
-     if (order === "true") {
-         console.log("order is true")
-         //if media query is 600px or less
-         if (window.innerWidth <= 600) {
-             animateScroll.scrollTo(1100)
-         }
-         //if media query is 600px or more
-         else {
-             animateScroll.scrollTo(1500)
-         }
-     }
+    if (order === "true") {
+        console.log("order is true")
+        //if media query is 600px or less
+        if (window.innerWidth <= 600) {
+            animateScroll.scrollTo(1100)
+        }
+        //if media query is 600px or more
+        else {
+            animateScroll.scrollTo(1500)
+        }
+    }
 
-     if (process.env.NEXT_PUBLIC_ENVIRONMENT === "production") {
-         if (!logged) {
-             logEvent(analytics, "page_view", {
-                 page_title: "Catering",
-                 page_location: "https://www.pistahouseirving.com/catering",
-                 page_path: "/catering",
-             })
-             setLogged(true)
-         }
-     } else {
-         if (!logged) {
-             console.log("development")
-             setLogged(true)
-         }
-     }
+    if (process.env.NEXT_PUBLIC_ENVIRONMENT === "production") {
+        if (!logged) {
+            logEvent(analytics, "page_view", {
+                page_title: "Catering",
+                page_location: "https://www.pistahouseirving.com/catering",
+                page_path: "/catering",
+            })
+            setLogged(true)
+        }
+    } else {
+        if (!logged) {
+            console.log("development")
+            setLogged(true)
+        }
+    }
     return (
         <>
             <Head>
                 <title>Catering | Pista House Irving</title>
             </Head>
             <section className="h-full">
-                <div className="fixed top-0  flex w-full flex-row items-center space-x-5 overflow-hidden overflow-x-scroll bg-pistaGray/70 pb-5 pr-5 pl-5 pt-20 backdrop-blur-lg md:pt-24   xl:justify-center">
+                <div className="fixed top-0 z-10  flex w-full flex-row items-center space-x-5 overflow-hidden overflow-x-scroll bg-pistaGray/70 pb-5 pr-5 pl-5 pt-20 backdrop-blur-lg md:pt-24   xl:justify-center">
                     <Link
                         to="VeggieAppetizers"
                         smooth={true}
@@ -300,18 +301,38 @@ function Catering() {
                     <h1 className=" mt-8 max-w-2xl text-4xl font-black  text-pistaGreen md:text-5xl xl:text-6xl">
                         Catering
                     </h1>
-                    <div>
+                    {/* <div>
                         <PhotoAlbum
                             layout="masonry"
                             photos={images}
                             //@ts-ignore
                             renderPhoto={renderPhoto}
                         />
+                    </div> */}
+
+                    <div className="z-0 mt-6 columns-2 gap-8 p-2 md:columns-4">
+                        {images.map((image, i) => (
+                            <div key={i} className="relative z-0">
+                                <Image
+                                    src={image.src}
+                                    alt={image.alt}
+                                    width={500}
+                                    height={500}
+                                    className="mb-6 rounded-lg object-cover"
+                                />
+                                <div className="absolute inset-0 flex items-center justify-center bg-black/70 opacity-0 transition-opacity duration-300 hover:opacity-100">
+                                    <p className="text-xl font-bold text-white">
+                                        {image.alt}
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
+
                 <div
                     id="menu"
-                    className="mx-auto -mt-32 max-w-screen-xl  px-8 pt-44 md:px-16"
+                    className="mx-auto  max-w-screen-xl  px-8  md:px-16"
                 >
                     <h1 className="mb-8  max-w-2xl text-4xl font-black  text-pistaGreen md:text-5xl xl:text-6xl">
                         Catering Menu
